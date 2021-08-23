4b30dad1267e462fefac3efa6b3f8f8e71642707
max <max.putilov@gmail.com>
2021-08-23 13:35:40 +0300

    initial commit

---
+++ apps/stripe-test/jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  displayName: 'stripe-test',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/stripe-test',
+};
